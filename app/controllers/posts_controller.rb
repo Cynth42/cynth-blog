1a858fcbd78e6e1a93241f63af5ece7aaa4a72c7
cynthia clinton <cynthiaclinton@cynthias-MBP.fios-router.home>
2017-03-12 00:04:33 -0500

    Edit and Delete Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
     end
     
     def new
-        @posts = Post.new
+        @post = Post.new
     end
     
     def create
@@ -14,13 +14,35 @@ class PostsController < ApplicationController
           redirect_to @posts
         else
         render 'new'
+        end
     end
 
     def show
         @post = Post.find(params[:id])
     end
     
+    def edit
+        @post = Post.find(params[:id])
+    end
     
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(params[:post].permit(:title, :body))
+          redirect_to @post
+        else
+          render 'edit'
+        end
+    end
+    
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+        
+    end
+    
+        
+
     private
        def post_params
          params.require(:post).permit(:title, :body)
