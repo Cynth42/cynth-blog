210ee408e4404a5b3bdee0d63eb508439157dada
cynthia clinton <cynthiaclinton@cynthias-MBP.fios-router.home>
2017-04-14 01:32:39 -0400

    Add Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,7 @@ class PostsController < ApplicationController
     def destroy
         @post = Post.find(params[:id])
         @post.destroy
-        redirect_to root_path
+        redirect_to posts_path
         
     end
     
